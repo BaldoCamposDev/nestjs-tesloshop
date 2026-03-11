f18ff0a40f3795269d128313b39e7f5526a14462
BaldoCampos <baldocamposg@gmail.com>
2026-03-10 19:36:39 -0700

    refactor: :recycle: Completed findAll, findOne and Remove functionality

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -41,20 +42,31 @@ export class ProductsService {
     }
   }
 
+  // TODO: paginar
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const { affected } = await this.productRepository.delete({ id });
+
+    if (affected === 0) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
   }
 
   private handleExceptions(error: any) {
