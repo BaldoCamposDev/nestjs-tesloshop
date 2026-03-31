3b7fee37406d913bb637efb7e373c0c333bca90d
BaldoCampos <baldocamposg@gmail.com>
2026-03-31 10:27:24 -0700

    feat: :sparkles: Log in functionality with hash comparison

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,14 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -33,6 +35,24 @@ export class AuthService {
     } catch (error) {
       this.handleExceptions(error);
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if (!user)
+      throw new UnauthorizedException(`Credentials are not valid (email)`);
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException(`Credentials are not valid (password)`);
+
+    return user;
+    // TODO return token
   }
 
   private handleExceptions(error: any): never {
