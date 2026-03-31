3fbb601d5591993eb0655f75ab1da81dafdf92a7
BaldoCampos <baldocamposg@gmail.com>
2026-03-31 15:53:06 -0700

    refactor: :recycle: Added endpoint responses for Swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,7 +16,7 @@ import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 import { User } from 'src/auth/entities/user.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Products')
 @Controller('products')
@@ -25,6 +25,9 @@ export class ProductsController {
 
   @Post()
   @Auth()
+  @ApiResponse({ status: 201, description: 'Product was created' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 403, description: 'Forbidden. Token related.' })
   create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
     return this.productsService.create(createProductDto, user);
   }
